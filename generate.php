972be6ea38aead5d6f9fa9338ed2bf470eb70c27
achmadrivaldi <mrtampan54@gmail.com>
2022-12-29 14:33:51 +0700

    update..

--- generate.php
+++ generate.php
@@ -4,14 +4,22 @@ $dataFolder = [];
 
 
 $dirs = glob('./*', GLOB_ONLYDIR);
-foreach ($dirs as $dir){
-    if(glob("*.mp3")){
-     $dataFolder[$dirs] = array("music" => glob("*.mp3")); 
+foreach ($dirs as $index => $dir){
+    $key = explode("/",$dir)[1];
+    $dataFolder["data"][$index][$key] = [];
+
+    if(glob($dir.'/*.mp3')){
+        echo $dir;
+        $allmusic = glob($dir.'/*.mp3');
+
+        foreach($allmusic as $idx => $music){
+            // if(empty($dataFolderp["data"][$index][$key])){
+            //     $dataFolder["data"][$index][$key] = [];
+            // }
+            array_push($dataFolder["data"][$index][$key],array("no" => $idx, "music" => explode("/",$music)[2]));
+        }
+        
     }
-}
-
-while($dirs = glob('./*', GLOB_ONLYDIR)) {
-
 }
 
 
